e710e6703929777c3c1dd0622cf6f478e9c0cf52
leahy8 <49131025+leahy8@users.noreply.github.com>
2020-01-13 18:50:18 -0500

    Update CY_Map2002-12.R

--- CY_Map2002-12.R
+++ CY_Map2002-12.R
@@ -1,21 +1,29 @@
 #Crop Yield Maps from 2002-2012 (code)
+#This code takes our collected crop yield data from the USDA and maps the data using a google maps API and a ggplots.
+#Since putting all of the data across our 11 year timeframe would be crowded and unhelpful, the crop yield data has been split into
+#each year using the subset command.
 
 #first we load in the csv file
 CropYield <-read.csv('/home/leahy8/Downloads/Crop_Yield_County_NorthernMW_2002-12_modified.csv')
 #We also need to call in the ggmap and mapping packages.
-library(ggmap)
-library(ggplot2)
-library(RColorBrewer)
+library(ggmap)        #For creating API maps
+library(ggplot2)      #For creating plots
+library(RColorBrewer) #For creating map aesthetics
 library(mapproj)
-#Now, we will cut this down to just 2002.
+#Now, we will cu down our crop yield data to just 2002.
 CY_2002 <- subset(CropYield, CropYield$Year == '2002')
-#Now, we need to get our API key, so we can use google maps.
+#Now, we need to register our API key from Google, so that we can use google maps as a base for our map.
 register_google(key = "API", write = TRUE)
-#To get all four states in one map, we need to pick a center point.
+#To get all four states in one map, we need to pick a center point.  After trial and error, Stevens Point, Wisconsin was chosen, since
+#it effectively illustrated all four states in our region with minimal cutoffs.
 NorthMW_center = as.numeric(geocode("Stevens Point, Wisconsin"))
+#Now, we create our map base with the ggmap command.  After trial and error, we used a zoom of 6.
 CY_2002_Mapbase <- ggmap(get_googlemap(center = NorthMW_center, zoom = 6, alpha = 0.5))
-#Now that we have a map, we can plot our points and scale them using ggplot
+#Now that we have a map, we can plot our points and scale them using ggplot.
+#First, we create a color vector to later use as a scale for our range of values.
 colormap <- c("Blue", "Green", "Yellow", "Red")
+#Now we can create a 2d summary map using the lattitude and longitude points from our data.
+#We also added a gradient scale to show the range of values on the map.
 CY_2002_Map <- CY_2002_Mapbase %+% CY_2002 +
   aes(x = Longitude, y = Latitude, z = Value, size = 10, alpha = 0.03) +
   stat_summary_2d()+
