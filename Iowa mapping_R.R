b1b0ff6b0acea9200c5c5b45525b0c7bbe2848c6
leahy8 <49131025+leahy8@users.noreply.github.com>
2020-01-13 19:20:13 -0500

    Update Iowa mapping_R.R

--- Iowa mapping_R.R
+++ Iowa mapping_R.R
@@ -1,17 +1,21 @@
-# First, we install the ggmap package
+#Iowa Mapping
+#This code was a demo of our exploration of mapping data in R using a google maps API.
+
+# First, we install the ggmap package, which will allow us to create maps.
 install.packages("ggmap")
 # and load the ggmap.
 library(ggmap)
-#Download Iowa Station Location Data
+#Next, we download the Iowa station location data.  The file location will change based on the user.
 Iowa_StationData <- read.csv('/home/lee3349/Downloads/IOWA.csv')
 head(Iowa_StationData)
-#Use Google API
-register_google(key = 'AIzaSyACb7Pmv9cAdE4HFSMTWyll5uMDXeErRyc', write = TRUE)
+#Use Google API.  The API will change based on the user.
+register_google(key = 'API', write = TRUE)
 Iowa_center = as.numeric(geocode("Iowa"))
-#Create station points
+#Then, we create a vector of station points to put on our map by taking the latitude and longitude points from the dataset.
 mypoints <- data.frame(lon=Iowa_StationData$longitude,lat=Iowa_StationData$latitude)
+#We then use the ggmap command to create our base map of Iowa.
 Iowa_Map <- ggmap(get_googlemap(center = Iowa_center,zoom=7))
 Iowa_Map
-#add station locations onto the map
+#Finally, we add the station locations onto our base map.
 Iowa_Map <- Iowa_Map + geom_point(data=mypoints, size=0.1)
 Iowa_Map
